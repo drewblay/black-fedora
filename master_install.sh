ff4bad58ac251f6f2c67f9342edf2c257d244d82
Andrew Tremblay <drewblay@gmail.com>
2018-11-07 19:48:51 -0500

    updated master with new scripts

--- master_install.sh
+++ master_install.sh
@@ -1,3 +1,10 @@
+# INFORMATION GATHERING
+Tools/dirb.sh
 Tools/nikto.sh
-Tools/metasploit.sh
+# nmap part of the metasploit install
+Tools/wireshark.sh
 Tools/zenmap.sh
+
+# EXPLOITATION TOOLS
+Tools/metasploit.sh
+# ncat part of the metasploit install
